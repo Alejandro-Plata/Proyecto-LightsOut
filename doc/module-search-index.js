13322b4780d83984a768bd653252923fff36d48e
Alejandro Plata Cortés <alejandroplatacortes@gmail.com>
2025-03-31 13:05:49 +0200

    Add files via upload

---
+++ doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"LightsOut"}];updateSearchResults();
---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"LightsOut","l":"LightsOut"}];updateSearchResults();
